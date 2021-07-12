52b129aa5fe60d207a75297b11e7482a36d24516
NasirHaidari <nasir.haidri@htomail.com>
2021-07-12 10:37:57 +0200

    somthing wrong with icon jumping

--- components/Nav.js
+++ components/Nav.js
@@ -9,7 +9,7 @@ export default function Nav() {
                {Object.entries(requests).map(([key, {title, url }])=> (
                    <h2 key={key}
                    onClick={()=> router.push(`/?genre${key}`)}
-                   className='last:pr-24 cursor-pointer transition duration-100 hover:scale-125 hover:text-white active:text-red-500'>{title}</h2>
+                   className='last:pr-24 cursor-pointer transition duration-100 hover:scale-125 hover:text-white active:text-red-500 '>{title}</h2>
                ) )}
            </div>
            <div className='absolute top-0 right-0 bg-gradient-to-l from-[#06202A] h-10 w-1/12'/>
